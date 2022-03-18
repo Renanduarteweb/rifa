1e018e49d5eda0cfa5e7be1189c4a7bb68ec851b
RenanLeviathan <renan.leviathan@gmail.com>
2022-03-18 10:13:19 -0300

    Atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RifaInputComponent } from './rifa-input/rifa-input.component';
@@ -16,7 +18,8 @@ import { MensagemComponent } from './mensagem/mensagem.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
